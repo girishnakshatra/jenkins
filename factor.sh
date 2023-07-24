d29dd16f138a0ddea42a62975441998441120db0
abc <abc@gmail.com>
2023-07-24 16:53:13 +0000

    modified

--- factor.sh
+++ factor.sh
@@ -3,10 +3,10 @@
 num=$1
 fact=1
 
-while [ $1 -ge 1 ]
+while [ $num -gt 1 ]
 do
-fact=`expr $fact \* $num`
-num=`expr $num - 1`
+        fact=`expr $fact \* $num`
+        num=`expr $num - 1`
 done
    echo "fact of $1 is $fact"
 
